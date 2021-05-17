3deed8e4f6e9fc3612f212665caa28c922cc79df
Damyan Doykov <77811406+Damyanmd@users.noreply.github.com>
2021-05-17 14:31:54 +0300

    Update database.py

--- rpcontacts/database.py
+++ rpcontacts/database.py
@@ -3,22 +3,10 @@ from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 
 def _createContactsTable():
-    """Create the contacts table in the database."""
     createTableQuery = QSqlQuery()
-    return createTableQuery.exec(
-        """
-        CREATE TABLE IF NOT EXISTS contacts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-            name VARCHAR(40) NOT NULL,
-            job VARCHAR(50),
-            email VARCHAR(40) NOT NULL,
-            phone VARCHAR(40) NOT NULL
-        )
-        """
-    )
+    return createTableQuery.exec()
 
 def createConnection(databaseName):
-    """Create and open a database connection."""
     connection = QSqlDatabase.addDatabase("QSQLITE")
     connection.setDatabaseName(databaseName)
 
