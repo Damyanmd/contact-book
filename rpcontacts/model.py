748823ba163aa4cdab9d09ab0861a78902cdffbf
Damyan Doykov <77811406+Damyanmd@users.noreply.github.com>
2021-05-17 14:33:10 +0300

    Update model.py

--- rpcontacts/model.py
+++ rpcontacts/model.py
@@ -5,9 +5,7 @@ class ContactsModel:
     def __init__(self):
         self.model = self._createModel()
 
-    @staticmethod
     def _createModel():
-        """Create and set up the model."""
         tableModel = QSqlTableModel()
         tableModel.setTable("contacts")
         tableModel.setEditStrategy(QSqlTableModel.OnFieldChange)
@@ -18,7 +16,6 @@ class ContactsModel:
         return tableModel
 
     def addContact(self, data):
-        """Add a contact to the database."""
         rows = self.model.rowCount()
         self.model.insertRows(rows, 1)
         for column, field in enumerate(data):
@@ -27,13 +24,11 @@ class ContactsModel:
         self.model.select()
 
     def deleteContact(self, row):
-        """Remove a contact from the database."""
         self.model.removeRow(row)
         self.model.submitAll()
         self.model.select()
 
     def clearContacts(self):
-        """Remove all contacts in the database."""
         self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
         self.model.removeRows(0, self.model.rowCount())
         self.model.submitAll()
